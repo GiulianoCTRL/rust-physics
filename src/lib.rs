91f38c59e6dd77a4189799d60205a49d8ee8312c
GiulianoCTRL <>
2020-12-04 14:05:46 +0100

    Started implementing numbers type

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,50 @@
+use std::ops::{Add, Sub, Mul, Div};
+use std::fmt;
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+pub enum Number {
+    Float(f64),
+    Int(i64),
+}
+
+impl fmt::Display for Number {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Number::Float(float) => float.fmt(f),
+            Number::Int(int) => int.fmt(f),
+        }
+    }
+}
+
+impl Number {
+    /// Determine if Number is of float type
+    pub fn is_float(&self) -> bool {
+        match self {
+            Number::Float(_) => true,
+            Number::Int(_) => false,
+        }
+    }
+
+    /// Determine if Number is of integer type
+    pub fn is_int(&self) -> bool {
+        !self.is_float()
+    }
+}
+
+impl Add for Number {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        match (self, other) {
+            (Number::Float(f0), Number::Float(f1)) => Number::Float(f0 + f1),
+            (Number::Float(f), Number::Int(i)) => Number::Float(f + i as f64),
+            (Number::Int(i), Number::Float(f)) => Number::Float(i as f64 + f),
+            (Number::Int(i0), Number::Int(i1)) => Number::Int(i0 + i1),
+        }
+    }
+
+}
+
 pub struct Velocity;
 
 // TODO: Everything
